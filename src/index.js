e0b4cad5dbc6097615b2df7f9049a20fad15e7d9
Yashwant Gaikwad <yashwantg9696@gmail.com>
2025-06-01 16:31:51 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectdb from "./db/connect.js";
+import {app} from './app.js';
 
 dotenv.config({
     path:'./env'
